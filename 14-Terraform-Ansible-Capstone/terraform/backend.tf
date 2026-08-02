db3e02121a721c00250ee0be2436bb543f4f0b6f
Asadul <how1998bd@gmail.com>
2026-08-02 10:21:22 +0100

    add new foleder 14

---
+++ 14-Terraform-Ansible-Capstone/terraform/backend.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.78.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    } 
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstateasadul001"
+    container_name       = "tfstatecapstone"
+    key                  = "dev.terraform.tfstate"
+  }
+
+  required_version = ">= 1.9.0"
+}
+
+provider "azurerm" {
+  features {}
+  use_cli = true
+}
