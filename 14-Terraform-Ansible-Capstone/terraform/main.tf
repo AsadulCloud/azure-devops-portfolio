8c55ae17b3047e566bf5330314cfee56c53f4a13
Asadul <how1998bd@gmail.com>
2026-08-02 15:26:52 +0100

    add latest files

--- 14-Terraform-Ansible-Capstone/terraform/main.tf
+++ 14-Terraform-Ansible-Capstone/terraform/main.tf
@@ -21,6 +21,8 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+  
+  depends_on = [azurerm_virtual_network.vnet]
 }
 # --- Public IP: this is the address Terraform will output, and Ansible will target ---
 resource "azurerm_public_ip" "vm_ip" {
