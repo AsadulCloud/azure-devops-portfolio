c9fc9a8c52bf0b0b1be0ef9d522abe0b3aa0b977
Asadul <how1998bd@gmail.com>
2026-08-02 10:47:29 +0100

    add new

--- 14-Terraform-Ansible-Capstone/terraform/outputs.tf
+++ 14-Terraform-Ansible-Capstone/terraform/outputs.tf
@@ -2,6 +2,7 @@
 # These are the two values the pipeline needs to hand off to Ansible:
 # 1. Where to connect (the IP)
 # 2. What key to connect with (the private key Terraform just generated)
+# ##########
 output "vm_public_ip" {
   description = "The public IP address of the virtual machine"
   value       = azurerm_public_ip.vm_ip.ip_address
