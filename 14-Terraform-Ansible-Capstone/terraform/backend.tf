56a010537cecd4f2378f1d8f15df78a1359dff80
Asadul <how1998bd@gmail.com>
2026-08-03 23:16:47 +0100

    add new files

--- 14-Terraform-Ansible-Capstone/terraform/backend.tf
+++ 14-Terraform-Ansible-Capstone/terraform/backend.tf
@@ -12,8 +12,8 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateasadul001"
-    container_name       = "tfstatecapstone"
+    storage_account_name = "tfstateasadul0001"
+    container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
 
