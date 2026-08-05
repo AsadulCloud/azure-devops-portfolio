0c5a3baf2a1f2f8e27e00f48bdd4577beded8614
Asadul <how1998bd@gmail.com>
2026-08-05 13:48:31 +0100

    add new all files

--- 10-Terraform-AKS-CICD/terraform/variables.tf
+++ 10-Terraform-AKS-CICD/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "project_name" {
     description = "Short name used as a prefix for all resources"
     type        = string
-    default     = "asadulcicd"    
+    default     = "portfolioasad"    
 }
 variable "location" {
     description = "Azure region to deploy resources"
@@ -31,7 +31,7 @@ variable "aks_node_count" {
 variable "aks_node_vm_size" {
     description = "VM size for the AKS node pool"
     type        = string
-    default     = "Standard_DS2_v3"
+    default     = "Standard_B2s_v2"
 }
 variable "aks_kubernetes_version" {
     description = "Kubernetes version for the AKS cluster"
